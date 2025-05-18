6a1590aec5b998751ce43a8814f368a37d487ce8
Adam Sorouri <adamsorouri8901@gmail.com>
2025-05-19 00:24:28 +0100

    Added icons

--- src/com/java/App/App.java
+++ src/com/java/App/App.java
@@ -1,9 +1,37 @@
 package com.java.App;
 
-public class App {
+import java.awt.Dimension;
+import java.awt.event.ActionListener;
 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+import com.java.form.App;
+import com.java.form.ButtonPanel;
+import com.java.form.GestionnaireNote;
+import com.java.form.LabeledTextField;
+
+@SuppressWarnings("serial")
+public class App extends JFrame implements ActionListener {
+	
+	boolean EnterAct = false;
+	ButtonPanel listOfButtons;
+	LabeledTextField tf1;
+	LabeledTextField tf2;
+	LabeledTextField tf3;
+	JLabel Statut = new JLabel("");
+	private GestionnaireNote g = new GestionnaireNote();
+	
 	public App() {
-		
+		exp03();
+		setTitle("Mon formulaire");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(1100, 800);
+		setMinimumSize(new Dimension(730, 600));
+		setLocationRelativeTo(null); // Centrer la fenetre principal
+		setVisible(true);
 	}
 
-}
+	public static void main(String[] args) {
+		new App();
+	}
